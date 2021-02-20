2ee047813428e74715518de49e2375689d02a39c
ot316 <olithompson@rocketmail.com>
2021-02-20 11:55:17 +0000

    finish tests for preprocessor

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ import unittest
 
 from part1_nn_lib import *
 
-# linear = LinearLayer(2, 3)
-# x = np.ones([4,2])
-# linear.forward(x)
-# grad_z = np.ones([4,3])
-# linear.backward(grad_z)
-# learning_rate = 1
-# linear.update_params(learning_rate)
+linear = LinearLayer(2, 3)
+x = np.ones([4,2])
+linear.forward(x)
+grad_z = np.ones([4,3])
+linear.backward(grad_z)
+learning_rate = 1
+linear.update_params(learning_rate)
 
 
 class TestPreProcessor(unittest.TestCase):
